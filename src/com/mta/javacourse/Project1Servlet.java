08ffd34d3c483868ce2be7068b91300bf0d3ffd9
arnit <arnit21@gmail.com>
2014-11-04 20:45:34 +0200

    Ex2

--- src/com/mta/javacourse/Project1Servlet.java
+++ src/com/mta/javacourse/Project1Servlet.java
@@ -5,7 +5,24 @@ import javax.servlet.http.*;
 @SuppressWarnings("serial")
 public class Project1Servlet extends HttpServlet {
 	public void doGet(HttpServletRequest req, HttpServletResponse resp) throws IOException {
-		resp.setContentType("text/plain");
-		resp.getWriter().println("Hello, Arnit Kashi !");
+		resp.setContentType("text/éhtml");
+
+		int num1;
+		int num2;
+		int num3;	
+
+		num1 = 4;
+		num2 = 3;
+		num3 = 7;
+		
+		
+		int var1 = num1 + num2;
+		int var2 = num3;
+		int result = (num1+num2) * num3;  
+
+		String resultStr = new String("<h1>Result of " +"("+num1+"+"+num2+")"+"*"+num3+"="+var1+"*"+var2+"="+result+"</h1>");
+				
+		resp.getWriter().println(resultStr);
+
 	}
 }
