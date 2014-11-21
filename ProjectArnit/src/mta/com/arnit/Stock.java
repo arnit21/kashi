aa1d8ded733d3c02483d3b23ce61cf419172a6a8
arnit <arnit21@gmail.com>
2014-11-21 18:07:21 +0200

    Ex04

--- ProjectArnit/src/mta/com/arnit/Stock.java
+++ ProjectArnit/src/mta/com/arnit/Stock.java
@@ -2,13 +2,14 @@ package mta.com.arnit;
 
 import java.io.IOException;
 import java.util.Date;
+import java.util.Calendar;
 
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 public class Stock extends HttpServlet {
-/*
+
 		private String symbol;
 		private float Ask;
 		private float Bid;
@@ -42,35 +43,33 @@ public class Stock extends HttpServlet {
 		}
 		
 		public String getHtmlDescription(){
-			 htmlDetailsString = "<b>stock symbol</b>: "+getsymbol()+ "<b>Ask</b>: "+getAsk()+ "<b>Bid</b>: "+getBid()+ "<b>Date</b>: "+getDate();
+			 htmlDetailsString = "<b>stock symbol</b>: "+getsymbol()+ "<b> Ask</b>: "+getAsk()+ "<b> Bid</b>: "+getBid()+ "<b> Date</b>: "+getDate();
 			return htmlDetailsString;
 		}
-*/
-		
+	
 		public void doGet(HttpServletRequest req, HttpServletResponse resp) throws IOException {
-			
-			/*
+
 			Stock s1 = new Stock();
-			//s1.setsymbol("PIH");
-			 s1.symbol = "PIH";
-			 s1.Ask = (float) 12.4;
-			 s1.Bid = (float) 13.1;
+			s1.setsymbol("PIH");
+			s1.setAsk((float)(12.4));
+			s1.setBid((float)(13.1));
+			s1.setDate(new Date());
 
-			 
 			Stock s2 = new Stock();
-			 s2.symbol = "AAL";
-			 s2.Ask = (float) 5.5;
-			 s2.Bid = (float) 5.78;
-
-			 
+			s2.setsymbol("AAL");
+			s2.setAsk((float)(5.5));
+			s2.setBid((float)(5.57));
+			s2.setDate(new Date());
+		
 			Stock s3 = new Stock();
-			 s3.symbol = "CAAS";
-			 s3.Ask = (float) 31.5;
-			 s3.Bid = (float) 31.2;
+			s3.setsymbol("CAAS");
+			s3.setAsk((float)(31.5));
+			s3.setBid((float)(31.2));
+			s3.setDate(new Date());
 			 
-			 resp.setContentType("text/html");
+			resp.setContentType("text/html");
 			resp.getWriter().println(s1.getHtmlDescription()+"<br>"+s2.getHtmlDescription()+"<br>"+s3.getHtmlDescription());
-			*/
+			
 		}	 
 }
 
